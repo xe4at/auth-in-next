333450eb654e7ddf1ca15d5011baf75caa821650
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-04-21 21:19:57 +0330

    create models

--- models/User.js
+++ models/User.js
@@ -1,1 +1,21 @@
 import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immmutable: true,
+  },
+});
+
+const User = models.User || model("User", userSchema);
+
+export default User;

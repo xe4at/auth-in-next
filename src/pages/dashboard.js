7235ac4da26558368f2c3d7320dcc47e0fd9f512
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-04-24 21:25:13 +0330

    Server-side authentication and page protection

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { redirect } from "next/dist/server/api-utils";
+import { verifyToken } from "../../utils/auth";
 
 function Dashboard() {
   return <div>Dashboard</div>;
 }
 
 export default Dashboard;
+
+export async function getServerSideProps(context) {
+  const { token } = context.req.cookies;
+  const secretKey = process.env.SECRET_KEY;
+
+  const result = verifyToken(token, secretKey);
+
+  if (!result)
+    return {
+      redirect: { destination: "/signin", permanent: false },
+    };
+
+  return { props: {} };
+}

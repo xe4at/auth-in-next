e00d980592f04e8a23e7fb68496219c1e3e7c4ac
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-04-25 16:57:37 +0330

    Displaying elements on the site based on authentication

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,48 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status == "success") setIsLoggedIn(true);
+      });
+  }, []);
+
   const signOutHandler = async () => {
     const res = await fetch("/api/auth/singout");
     const data = await res.json();
-    console.log(data);
+    if (data.status == "success") setIsLoggedIn(true);
   };
   return (
     <div className="home-container">
       <h1 className="home-title">Welcome to Our App</h1>
+
       <div className="home-buttons">
-        <Link href="/dashboard" className="home-button">
-          <span>Dashboard</span>
-        </Link>
-        <Link href="/singup" className="home-button">
-          <span>Create Account</span>
-        </Link>
-        <Link href="/singin" className="home-button">
-          <span>Sign In</span>
-        </Link>
-        <button onClick={signOutHandler} className="home-button">
-          Sign Out
-        </button>
+        {isLoggedIn ? (
+          <>
+            <Link href="/dashboard" className="home-button">
+              <span>Dashboard</span>
+            </Link>
+            <button onClick={signOutHandler} className="home-button">
+              Sign Out
+            </button>
+          </>
+        ) : null}
+
+        {!isLoggedIn ? (
+          <>
+            <Link href="/singup" className="home-button">
+              <span>Create Account</span>
+            </Link>
+            <Link href="/singin" className="home-button">
+              <span>Sign In</span>
+            </Link>
+          </>
+        ) : null}
       </div>
     </div>
   );
